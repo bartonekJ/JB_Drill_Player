f73d4f5423bc07acb77797ec171a545dbee5ab7f
bartonekJ <bartonek@users.noreply.github.com>
2026-08-20 00:55:28 +0200

    Deploy JB Drill Player

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "jb-drill-player-v2";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./player.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icons/finesse-shapes.js",
+  "./icons/JB_Logo.svg",
+  "./icons/app-icon-192.png",
+  "./icons/app-icon-512.png",
+  "./icons/Puck.png",
+  "./icons/Pylon_Ice.png",
+  "./icons/Net.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
