c67bf1e4e180fa82090e8adb4de5a89e123414fc
Jirka Bartonek <jirka.bartonek@gmail.com>
2026-08-20 12:37:23 +0200

    Load expiring short Player links

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jb-drill-player-v2";
+const CACHE_NAME = "jb-drill-player-v3";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -31,6 +31,11 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith("/api/")) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
   event.respondWith(
     fetch(event.request)
       .then((response) => {
