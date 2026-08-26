0ae0c25a04300feb6f5fd805b59d3c3de5f4244a
Jirka Bartonek <jirka.bartonek@gmail.com>
2026-08-26 22:34:09 +0200

    Bust cached Player assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jb-drill-player-v4";
+const CACHE_NAME = "jb-drill-player-v5";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -16,7 +16,9 @@ const APP_SHELL = [
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(
+    APP_SHELL.map((url) => new Request(url, { cache: "reload" }))
+  )));
   self.skipWaiting();
 });
 
